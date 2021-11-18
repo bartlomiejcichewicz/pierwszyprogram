9a3a83ed9f49235e9dc255114c558919e0d1269c
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-18 21:22:11 +0100

    Operatory-Zadanie2

--- Program.cs
+++ Program.cs
@@ -6,21 +6,10 @@ namespace PierwszyProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj swoje imię:");
-            var name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
-            int number = 0;
-            float money = 0;
-            string text = "";
-            bool isLogged = true;
-            char myChar = ' ';
-            decimal price = 0;
-            Console.WriteLine(number);
-            Console.WriteLine(money);
-            Console.WriteLine(text);
-            Console.WriteLine(isLogged);
-            Console.WriteLine(myChar);
-            Console.WriteLine(price);
+            string myAge = "Age: ";
+            int wifeAge = 18;
+            string result = myAge + wifeAge;
+            Console.WriteLine(result);
         }
     }
 }
