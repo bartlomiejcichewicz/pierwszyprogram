fdbe99ec835b08ec77a04f9e4769058c4852a158
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-18 21:05:42 +0100

    Rozgrzewka

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PierwszyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoje imię:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            int result = 5 + 9;
+        }
+    }
+}
