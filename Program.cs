3559e51909f7fb1d5ca83846458cdf977e1334e1
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-19 17:37:10 +0100

    Instrukcje Sterujace - Zadanie 5

--- Program.cs
+++ Program.cs
@@ -6,13 +6,22 @@ namespace PierwszyProgram
     {
         static void Main(string[] args)
         {
-            int n = 5;
-            for (int i = 1; i <= n; i++)
-            {
-                for (int j = 1; j <= i; j++)
-                    Console.Write("* ");
-                Console.WriteLine();
-            }
+            int exam = 69;
+            int point = exam;
+            if (point >= 0 && point < 40)
+                Console.WriteLine("Ocena Nieodstateczna");
+            else if (point < 55)
+                Console.WriteLine("Ocena Dopuszczająca");
+            else if (point < 70)
+                Console.WriteLine("Ocena Dostateczna");
+            else if (point < 85)
+                Console.WriteLine("Ocena Dobra");
+            else if (point < 99)
+                Console.WriteLine("Ocena Bardzo Dobra");
+            else if (point < 100)
+                Console.WriteLine("Ocena Celująca");
+            else
+                Console.WriteLine("Wartość poza zakresem");
         }
     }
 }
