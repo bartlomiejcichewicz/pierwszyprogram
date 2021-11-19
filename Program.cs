2c4d3819e1496a7063962c0a3dde2a5f84ab8ee7
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-19 17:15:57 +0100

    Instrukcje Sterujace - Zadanie 2

--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@ namespace PierwszyProgram
     {
         static void Main(string[] args)
         {
-            int n1 = 10, n2 = 20;
-            if (n1 > n2)
-                Console.WriteLine("n1 jest większe od n2");
-            else if (n2 > n1)
-                Console.WriteLine("n2 jest większe od n1");
-            else
-                Console.WriteLine("n1 jest równe n2");
+            for (int i = 0; i < 10; i++)
+                Console.Write("C# ");
+            int j = 0;
+            while (j < 10)
+            {
+                Console.Write("C# ");
+                j++;
+            }
         }
     }
 }
