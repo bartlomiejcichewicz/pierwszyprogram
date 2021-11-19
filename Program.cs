4fef958c74702a758e90ec27e2dbbed59719630b
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-19 17:45:03 +0100

    Kolekcje - Zadanie 1

--- Program.cs
+++ Program.cs
@@ -6,22 +6,9 @@ namespace PierwszyProgram
     {
         static void Main(string[] args)
         {
-            int exam = 69;
-            int point = exam;
-            if (point >= 0 && point < 40)
-                Console.WriteLine("Ocena Nieodstateczna");
-            else if (point < 55)
-                Console.WriteLine("Ocena Dopuszczająca");
-            else if (point < 70)
-                Console.WriteLine("Ocena Dostateczna");
-            else if (point < 85)
-                Console.WriteLine("Ocena Dobra");
-            else if (point < 99)
-                Console.WriteLine("Ocena Bardzo Dobra");
-            else if (point < 100)
-                Console.WriteLine("Ocena Celująca");
-            else
-                Console.WriteLine("Wartość poza zakresem");
+            string[] colors = { "zielony", "niebieski", "różowy", "czarny", "fioletowy" };
+            Console.WriteLine("Mój pierwszy kolor to: " + colors[0]);
+            Console.WriteLine("Mój ostatni kolor to: " + colors[colors.Length - 1]);
         }
     }
 }
